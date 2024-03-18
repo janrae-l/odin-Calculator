ac693c6cb7adf25ff73b25222a596bf2cd07ab53
janrae-l <105533574+janrae-l@users.noreply.github.com>
2024-03-18 15:48:07 +0800

    Make operate func to do operations in two passed nums and the operate

--- javascript.js
+++ javascript.js
@@ -14,3 +14,8 @@ const divide = function (a, b) {
 const firstNum = 0;
 const secNum = 0;
 const operator = "";
+
+const operate = function (a, operate, b) {
+  //Takes numbers a and b
+  //Use the operate param to figure out what operations are to be done in numbers a and b
+};
