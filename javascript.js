4436018f18ec31e48c10f14cd091fc898fa6315e
janrae-l <105533574+janrae-l@users.noreply.github.com>
2024-03-18 15:45:08 +0800

    Add three variables for two numbers and an operator

---
+++ javascript.js
@@ -0,0 +1,16 @@
+const add = function (a, b) {
+  return a + b;
+};
+const subtract = function (a, b) {
+  return a - b;
+};
+const multiply = function (a, b) {
+  return a * b;
+};
+const divide = function (a, b) {
+  return a / b;
+};
+
+const firstNum = 0;
+const secNum = 0;
+const operator = "";
