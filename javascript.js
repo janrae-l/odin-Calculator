9e6f72596b2faf93b605673c02328be9462c6c5e
janrae-l <105533574+janrae-l@users.noreply.github.com>
2024-03-18 15:58:12 +0800

    Add function body for the operate func

--- javascript.js
+++ javascript.js
@@ -15,7 +15,16 @@ const firstNum = 0;
 const secNum = 0;
 const operator = "";
 
-const operate = function (a, operate, b) {
+const operate = function (a, b, operate) {
   //Takes numbers a and b
   //Use the operate param to figure out what operations are to be done in numbers a and b
+  if (operate === "+") {
+    add(a, b);
+  } else if (operate === "-") {
+    subtract(a, b);
+  } else if (operate === "*") {
+    multiply(a, b);
+  } else if (operate === "/") {
+    divide(a, b);
+  }
 };
